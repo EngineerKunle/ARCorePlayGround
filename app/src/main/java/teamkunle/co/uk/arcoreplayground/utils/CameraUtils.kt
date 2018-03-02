c7750be9bbb7ee1c882010e243d540856cd8c2e7
Kunle Ogunjimi <kunle.ogunjimi@telegraph.co.uk>
2018-03-02 23:45:18 +0000

    camera utils working : minor refactor

--- app/src/main/java/teamkunle/co/uk/arcoreplayground/utils/CameraUtils.kt
+++ app/src/main/java/teamkunle/co/uk/arcoreplayground/utils/CameraUtils.kt
@@ -11,27 +11,24 @@ import android.support.v4.content.ContextCompat
 
 const val  cameraPermission = Manifest.permission.CAMERA
 const val cameraPermissionCode = 1
-class CameraUtils {
+object CameraUtils {
 
-    companion object {
+    fun hasCameraPermission(activity: Activity) : Boolean {
+        return ContextCompat.checkSelfPermission(activity, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+    }
 
-        fun hasCameraPermission(activity: Activity) : Boolean {
-            return ContextCompat.checkSelfPermission(activity, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
-        }
+    fun shouldRequestCameraPermission(activity: Activity) : Boolean {
+        return ActivityCompat.shouldShowRequestPermissionRationale(activity, cameraPermission)
+    }
 
-        fun shouldRequestCameraPermission(activity: Activity) : Boolean {
-            return ActivityCompat.shouldShowRequestPermissionRationale(activity, cameraPermission)
-        }
+    fun requestCameraPermission(activity: Activity) {
+        ActivityCompat.requestPermissions(activity, arrayOf(cameraPermission), cameraPermissionCode)
+    }
 
-        fun requestCameraPermission(activity: Activity) {
-            ActivityCompat.requestPermissions(activity, arrayOf(cameraPermission), cameraPermissionCode)
-        }
-
-        fun launchPermissionSettings(activity: Activity) {
-            val intent = Intent()
-            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
-            intent.data   = Uri.fromParts("package", activity.packageName, null)
-            activity.startActivity(intent)
-        }
+    fun launchPermissionSettings(activity: Activity) {
+        val intent = Intent()
+        intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+        intent.data   = Uri.fromParts("package", activity.packageName, null)
+        activity.startActivity(intent)
     }
 }
